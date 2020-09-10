8de5a3fb07f0b25323697b2639ebe8462dd98696
Hyeonguk Bahk <bahkhyeonguk@gmail.com>
2020-09-10 17:50:19 +0900

    update

--- _posts/2020-09-10-hello-world.md
+++ _posts/2020-09-10-hello-world.md
@@ -1,1 +1,23 @@
+---
+title: The First Post
+layout: single
+author_profile: true
+read_time: true
+comments: true
+share: true
+related: true
+categories:
+- etc
+tag:
+- first
+description:
+article_tag1:
+article_section:
+meta_keywords: 
+last_modified_at: 
+toc: false
+toc_sticky: true
+toc_label: 목차
+---
+
 Hello, World!
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Hello World",
-        "excerpt":"Hello, World!  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/hello-world/",
+        "title": "The First Post",
+        "excerpt":"Hello, World!  ","categories": ["etc"],
+        "tags": ["first"],
+        "url": "http://localhost:4000/etc/hello-world/",
         "teaser": null
       }]
