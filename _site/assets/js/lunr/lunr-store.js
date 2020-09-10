401e141779102686b53005954d5cc3e05d559fe5
Hyeonguk Bahk <bahkhyeonguk@gmail.com>
2020-09-10 15:34:10 +0900

    remove unnecessary files

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Hello World",
+        "excerpt":"Hello, World!  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/hello-world/",
+        "teaser": null
+      }]
